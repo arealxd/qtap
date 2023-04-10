b7bbd47060d50c2af60635464efa33d06962ce8b
arealxd <metorikz@gmail.com>
2023-04-10 21:32:05 +0600

    Added PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,34 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import { VitePWA } from "vite-plugin-pwa";
+
+const vitePWA = VitePWA({
+  registerType: "autoUpdate",
+  outDir: "build",
+  manifest: {
+    name: "QTap",
+    short_name: "QTap",
+    description: "QTap",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "assets/images/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "assets/images/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), vitePWA],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+export default {
+  globDirectory: "build/",
+  globPatterns: [
+    "**/*/{html,js,css,woff2,ico,png,jpg,svg,js,jpeg,gif,webp,woff,ttf,eot,otf,mp4,webm,mp3,ogg,txt,xml,json}",
+  ],
+  swDest: "build/sw.js",
+};
